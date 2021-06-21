37ae01b9c80be3a0b0653920307e6f7d1b4cb7e3
Ralf Prezia <62730168+ralfprezia@users.noreply.github.com>
2021-06-21 12:57:07 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function Button() {
+    //let counter = 0;
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <button onClick={increment}>
+            {counter}
+        </button>
+    )
+}
+// named export
